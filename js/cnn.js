4f0a41015d324fb4c3d022be82f5789cef3d6174
Cameron <cameronboyce93@gmail.com>
2017-01-18 17:54:11 -0500

    added button

--- js/cnn.js
+++ js/cnn.js
@@ -29,6 +29,25 @@ function introElement(tag, time){
   }, time);
 }
 
+$(function() {
+                $('.slidebttn').hover(
+					function () {
+						var $this 		= $(this);
+						var $slidelem 	= $this.prev();
+						$slidelem.stop().animate({'width':'225px'},300);
+						$slidelem.find('span').stop(true,true).fadeIn();
+						$this.addClass('button_c');
+					},
+					function () {
+						var $this 		= $(this);
+						var $slidelem 	= $this.prev();
+						$slidelem.stop().animate({'width':'70px'},200);
+						$slidelem.find('span').stop(true,true).fadeOut();
+						$this.removeClass('button_c');
+					}
+				);
+            });
+
 /*********************************************************************
 Page Load
 *********************************************************************/
