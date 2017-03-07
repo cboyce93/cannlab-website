c05865df0cd461d400ecb7eb2af3e436f1fba9b1
Cameron <cameronboyce93@gmail.com>
2017-03-07 11:44:13 -0500

    php form

--- form.php
+++ form.php
@@ -1,3 +1,11 @@
-<?php
-  echo 'hello world!';
+<?php 
+$first_name = $_POST['first_name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "cameronboyce93@gmail.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
 ?>
